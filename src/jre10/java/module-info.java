94ca86b7efe5f66759327cc7d8719ab90ee952ce
GedMarc <ged_marc@hotmail.com>
2018-07-26 23:59:01 +0200

    JDK 10 Exposures and operations

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -17,5 +17,9 @@ module com.jwebmp.plugins.metrojs {
 	requires com.jwebmp.plugins.easingeffects;
 
 	provides IPageConfigurator with JQMetroPageConfigurator;
-
+	opens com.jwebmp.plugins.metrojs to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.interfaces to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.enumerations to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.references to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.tiles to com.fasterxml.jackson.databind,com.jwebmp.core;
 }
