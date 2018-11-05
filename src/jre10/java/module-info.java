22d737563d3e77a3afa58e3c1c5be9561de6130b
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:27 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,4 +1,6 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.metrojs.JQMetroPageConfigurator;
 
 module com.jwebmp.plugins.metrojs {
@@ -15,11 +17,16 @@ module com.jwebmp.plugins.metrojs {
 	requires java.validation;
 	requires java.logging;
 	requires com.jwebmp.plugins.easingeffects;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with JQMetroPageConfigurator;
-	opens com.jwebmp.plugins.metrojs to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.metrojs.interfaces to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.metrojs.enumerations to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.metrojs.references to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.metrojs.tiles to com.fasterxml.jackson.databind,com.jwebmp.core;
+
+	provides IGuiceScanJarExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
+	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
+
+	opens com.jwebmp.plugins.metrojs to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.enumerations to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.references to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.metrojs.tiles to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
---
+++ src/main/java/com/jwebmp/plugins/metrojs/implementations/JQMetroExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.metrojs.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JQMetroExclusionsModule
+		implements IGuiceScanModuleExclusions<JQMetroExclusionsModule>,
+				           IGuiceScanJarExclusions<JQMetroExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-metro-tiles-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.metrojs");
+		return strings;
+	}
+}
