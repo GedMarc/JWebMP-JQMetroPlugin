567888bb4bd9567de74ac8780f7f1d55c78bfa6b
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:24 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -6,18 +6,18 @@ module com.jwebmp.plugins.metrojs {
 	exports com.jwebmp.plugins.metrojs.tiles;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
 	requires com.jwebmp.plugins.easingeffects;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.metrojs.JQMetroPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.metrojs.implementations.JQMetroExclusionsModule;
 
 	opens com.jwebmp.plugins.metrojs to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.metrojs.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
